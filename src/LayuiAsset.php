0adb31fbaeeee5239b119dc5d4605dd679ea80fd
CeeFee <383818686@qq.com>
2020-05-09 10:19:01 +0800

    fixed bug

--- src/LayuiAsset.php
+++ src/LayuiAsset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 
 class LayuiAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/ceefee/yii2-layui/layui/dist';
+    public $sourcePath = '@vendor/ceefee/yii2-layui/layui';
     public $css = [
         'css/layui.css'
     ];
--- src/LayuiPluginAsset.php
+++ src/LayuiPluginAsset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 
 class LayuiPluginAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/ceefee/yii2-layui/layui/dist';
+    public $sourcePath = '@vendor/ceefee/yii2-layui/layui';
     public $js = [
         'layui.js',
     ];
